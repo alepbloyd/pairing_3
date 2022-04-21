a79ffabc14ef0da0e1ff8d0c73d8fa85a77ec2a7
alepbloyd <alexanderleeboyd@gmail.com>
2022-04-21 16:30:48 -0400

    Initial commit

---
+++ hash_practice.rb
@@ -0,0 +1,13 @@
+person = {
+  "name" => "Alex",
+  "age" => 29,
+  "glasses" => true,
+  "hair" => "dark brown"
+}
+
+# puts person
+#
+# puts person["name"]
+
+puts person.keys[0]
+puts person.values[0] 
